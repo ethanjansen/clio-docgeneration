80af0d19544a732199b7deb229070555cbb437fc
Ethan Jansen <ethan.jansen@wallawalla.edu>
2024-09-19 10:57:08 -0700

    document generation

--- doc.py
+++ doc.py
@@ -1,15 +1,31 @@
 from docxtpl import DocxTemplate, RichText
 
-# template to use
-doc = DocxTemplate("template-test.docx")
+######## Functions ########
+def generate_doc(templateDocx: str, outputDocx: str, context: dict) -> None:
+    # template to use
+    doc = DocxTemplate(templateDocx)
 
-# variables to replace in the template
-context = { "user": "Ethan Jansen",
-            "user_bold": RichText("Ethan Jansen", bold=True),
-            "date": "2021<>09<>01",
-            "parType": "",
-            }
-doc.render(context, autoescape=True)
+    # variables to replace in the template
+    doc.render(context, autoescape=True)
 
-# save the generated document
-doc.save("generated-doc.docx")
+    # save the generated document
+    doc.save(outputDocx)
+
+
+
+####### Tests #######
+def test_doc():
+    print("Testing doc.py")
+    # variables to replace in test1-template.docx
+    context1 = { "user": "Ethan Jansen",
+                "user_bold": RichText("Ethan Jansen", bold=True),
+                "date": "2021<>09<>01",
+                "parType": "",
+                }
+    generate_doc("test1_template.docx", "test1_generated.docx", context1)
+
+
+####### Main #######
+if __name__ == "__main__":
+    print("Invoking doc.py directly. Running tests:")
+    test_doc()
