9326ec6219572a2ffca4719572b519fffcf1ddf7
Ethan Jansen <ethan.jansen@wallawalla.edu>
2024-09-19 12:21:13 -0700

    main

---
+++ main.py
@@ -0,0 +1,20 @@
+# Main for docGeneration/main.py:
+from translate_io import translate_io
+from doc import generate_doc
+
+#### Functions ####
+def main():
+    print("Running proof of concept")
+
+    # run test1
+    input = {"username": "Ethan Jansen",
+             "person": "Not Ethan Jansen",
+             "date": "2021-09-01",
+             "par_type": "second",
+             }
+    generate_doc("test1_template.docx", "test1_generated.docx", translate_io(input, "test1_mapping.json"))
+
+
+#### Main ####
+if __name__ == "__main__":
+    main()
